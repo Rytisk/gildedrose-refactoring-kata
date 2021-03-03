6c423ffe7534f785d938460f15d0413183e7ab31
Rytis Kaplūnas <rytis.kap@gmail.com>
2021-03-03 21:03:48 +0200

    Cleanup

--- GildedRoseKata.Tests/GildedRoseTest.cs
+++ GildedRoseKata.Tests/GildedRoseTest.cs
@@ -1,17 +1,6 @@
-﻿using Xunit;
-using System.Collections.Generic;
-
-namespace GildedRoseKata.Tests
+﻿namespace GildedRoseKata.Tests
 {
     public class GildedRoseTest
     {
-        [Fact]
-        public void foo()
-        {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.Equal("fixme", Items[0].Name);
-        }
     }
 }
