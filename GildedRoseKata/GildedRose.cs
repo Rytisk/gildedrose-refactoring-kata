107f0d019965607c5505990e44f5b6f82b08309b
Rytis Kaplūnas <rytis.kap@gmail.com>
2021-03-03 22:14:18 +0200

    Extract code for Aged Brie

--- GildedRoseKata/GildedRose.cs
+++ GildedRoseKata/GildedRose.cs
@@ -11,11 +11,33 @@ namespace GildedRoseKata
             _items = Items;
         }
 
+        private void UpdateAgedBrie(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
+
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0 && item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
+        }
+
         public void UpdateQuality()
         {
             for (var i = 0; i < _items.Count; i++)
             {
-                if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (_items[i].Name == "Aged Brie")
+                {
+                    UpdateAgedBrie(_items[i]);
+
+                    continue;
+                }
+
+                if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (_items[i].Quality > 0)
                     {
@@ -59,29 +81,19 @@ namespace GildedRoseKata
 
                 if (_items[i].SellIn < 0)
                 {
-                    if (_items[i].Name != "Aged Brie")
+                    if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (_items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (_items[i].Quality > 0)
                         {
-                            if (_items[i].Quality > 0)
+                            if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (_items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    _items[i].Quality = _items[i].Quality - 1;
-                                }
+                                _items[i].Quality = _items[i].Quality - 1;
                             }
-                        }
-                        else
-                        {
-                            _items[i].Quality = _items[i].Quality - _items[i].Quality;
                         }
                     }
                     else
                     {
-                        if (_items[i].Quality < 50)
-                        {
-                            _items[i].Quality = _items[i].Quality + 1;
-                        }
+                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
                     }
                 }
             }
