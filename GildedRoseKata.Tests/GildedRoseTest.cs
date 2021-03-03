79c92f03279166b0924f64f9d5fc8e7b4ddb9a7c
Rytis Kaplūnas <rytkaplunas@gmail.com>
2021-03-03 23:35:51 +0200

    GildedRose unit test

--- GildedRoseKata.Tests/GildedRoseTest.cs
+++ GildedRoseKata.Tests/GildedRoseTest.cs
@@ -1,7 +1,25 @@
-﻿namespace GildedRoseKata.Tests
+﻿using Moq;
+using Xunit;
+
+namespace GildedRoseKata.Tests
 {
     public class GildedRoseTest
     {
-        
+        [Fact]
+        public void ShouldCallUpdateOnItemIfImplementsIUpdatable()
+        {
+            // arrange
+            var item = new Mock<Item>();
+            var updatableItem = item.As<IUpdatable>();
+
+            var gildedRose = new GildedRose(new[] { item.Object });
+            // act
+            gildedRose.UpdateQuality();
+
+            // assert
+            updatableItem.Verify(
+                u => u.Update(),
+                Times.Once());
+        }
     }
 }
