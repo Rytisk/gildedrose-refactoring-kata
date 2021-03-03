34d21b78171a76ea690225d8ccaf1cfa021cf58b
Rytis Kaplūnas <rytis.kap@gmail.com>
2021-03-03 21:41:35 +0200

    Unit tests for Sulfuras

--- GildedRoseKata.Tests/GildedRoseTest.cs
+++ GildedRoseKata.Tests/GildedRoseTest.cs
@@ -130,5 +130,41 @@ namespace GildedRoseKata.Tests
             // assert
             Assert.Equal(maximumQuality, item.Quality);
         }
+
+        [Fact]
+        public void ShouldNotDegradeSulfurasQuality()
+        {
+            // arrange
+            var quality = 80;
+            var sellIn = 10;
+
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = quality };
+
+            var gildedRose = new GildedRose(new[] { item });
+
+            // act
+            gildedRose.UpdateQuality();
+
+            // assert
+            Assert.Equal(quality, item.Quality);
+        }
+
+        [Fact]
+        public void ShouldNotAlterSulfurasSellInTime()
+        {
+            // arrange
+            var quality = 80;
+            var sellIn = 10;
+
+            var item = new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = sellIn, Quality = quality };
+
+            var gildedRose = new GildedRose(new[] { item });
+
+            // act
+            gildedRose.UpdateQuality();
+
+            // assert
+            Assert.Equal(sellIn, item.SellIn);
+        }
     }
 }
