c1289ab58b9fd45a4ab43f8f485e431adb3111b6
Rytis Kaplūnas <rytkaplunas@gmail.com>
2021-03-04 20:01:35 +0200

    ConjuredItem updated according to the requirements

--- GildedRoseKata/Items/ConjuredManaCakeItem.cs
+++ GildedRoseKata/Items/ConjuredManaCakeItem.cs
@@ -1,10 +1,33 @@
 ﻿namespace GildedRoseKata.Items
 {
-    public class ConjuredManaCakeItem : DegradingItem
+    public class ConjuredManaCakeItem : Item, IUpdatable
     {
         public ConjuredManaCakeItem(int sellIn, int quality)
-            : base("Conjured Mana Cake", sellIn, quality)
         {
+            Name = "Conjured Mana Cake";
+            SellIn = sellIn;
+            Quality = quality;
+        }
+
+        public void Update()
+        {
+            DecreaseQualityBy(2);
+
+            SellIn -= 1;
+
+            if (SellIn < 0)
+            {
+                DecreaseQualityBy(2);
+            }
+        }
+
+        private void DecreaseQualityBy(int value)
+        {
+            var decreasedValue = Quality - value;
+
+            Quality = decreasedValue < 0 
+                ? 0
+                : decreasedValue;
         }
     }
 }
